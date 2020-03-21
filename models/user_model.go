81d2b02f368f916286391ba3d02704d99383452c
Nick <naaltunian@icloud.com>
2020-03-20 20:23:47 -0400

    Refactor UserModel

--- models/user_model.go
+++ models/user_model.go
@@ -1,14 +1,20 @@
 package models
 
-import "time"
+import (
+	"time"
 
+	"go.mongodb.org/mongo-driver/bson/primitive"
+)
+
+// UserModel for User
 type UserModel struct {
-	Name           string    `json:"name,omitempty"`
-	Email          string    `json:"email,omitempty"`
-	Password       string    `json:"password,omitempty"`
-	LinkedIn       string    `json:"linkedIn,omitempty"`
-	GithubUsername string    `json:"githubUsername,omitempty"`
-	PersonalSite   string    `json:"personalSite,omitempty"`
-	Bio            string    `json:"bio,omitempty"`
-	DateCreated    time.Time `json:"dateCreated,omitempty"`
+	ID             primitive.ObjectId `json:"_id,omitempty" bson:"_id, omitempty"`
+	Name           string             `json:"name,omitempty" bson:"name,omitempty"`
+	Email          string             `json:"email,omitempty" bson:"email,omitempty"`
+	Password       string             `json:"password,omitempty" bson:"password,omitempty"`
+	LinkedIn       string             `json:"linkedIn,omitempty" bson:"linkedIn,omitempty"`
+	GithubUsername string             `json:"githubUsername,omitempty" bson:"githubUsername,omitempty"`
+	PersonalSite   string             `json:"personalSite,omitempty" bson:"personalSite,omitempty"`
+	Bio            string             `json:"bio,omitempty" bson:"bio,omitempty"`
+	DateCreated    time.Time          `json:"dateCreated,omitempty" bson:"dateCreated,omitempty"`
 }
